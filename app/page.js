fa241bb52f47bb0a7cc4b88faaf27c8f1b567f2c
Priyansh Prajapat <oyepriyansh@outlook.com>
2025-08-28 19:15:00 +0530

    init nexjs

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+
+    </div>
+  );
+}
